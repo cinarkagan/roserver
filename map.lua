3de0695a6e67ff9fb8de8b753e5e5c29a33ac54d
cinarkagan <68916102+cinarkagan@users.noreply.github.com>
2022-12-24 19:47:59 +0300

    Update map.lua

--- map.lua
+++ map.lua
@@ -2,7 +2,27 @@ local Baseplate = Instance.new('Part')
 Baseplate.Name = 'Baseplate'
 Baseplate.Anchored = true
 Baseplate.CanCollide = true
-Baseplate.Size = Vector3.new(1756.770263671875,20,2048)
-Baseplate.BrickColor = BrickColor.new('Dark stone grey')
-Baseplate.Position = Vector3.new(0,0,0)
+Baseplate.Size = Vector3.new(2048,16,2048)
+Baseplate.BrickColor = BrickColor.new('Dark grey metallic')
+Baseplate.Position = Vector3.new(0,-8,0)
 Baseplate.Parent = workspace
+
+
+local SpawnLocation = Instance.new('Part')
+SpawnLocation.Name = 'SpawnLocation'
+SpawnLocation.Anchored = true
+SpawnLocation.CanCollide = true
+SpawnLocation.Size = Vector3.new(12,1,12)
+SpawnLocation.BrickColor = BrickColor.new('Medium stone grey')
+SpawnLocation.Position = Vector3.new(0,0.5,0)
+SpawnLocation.Parent = workspace
+
+
+local Part = Instance.new('Part')
+Part.Name = 'Part'
+Part.Anchored = false
+Part.CanCollide = true
+Part.Size = Vector3.new(3.9599997997283936,3.926516532897949,3.6646013259887695)
+Part.BrickColor = BrickColor.new('Really red')
+Part.Position = Vector3.new(2.0383005142211914,1.963261604309082,-18.049184799194336)
+Part.Parent = workspace
