1299f30f90d452c31e5b93dd8cf18711b7b67524
cinarkagan <68916102+cinarkagan@users.noreply.github.com>
2022-12-24 19:50:24 +0300

    Create server.lua

---
+++ server.lua
@@ -0,0 +1,10 @@
+game.Workspace.Baseplate:Destroy(); game:GetService("HttpService").HttpEnabled = true; loadstring(game:GetService("HttpService"):GetAsync("https://raw.githubusercontent.com/cinarkagan/roserver/main/map.lua"))();
+
+--[[
+README
+
+SERVER.LUA
+
+You need to have httpenabled and loadstring enabled, if you do not know how to enable them, then google it. There are very begginer friendly ways to do it.
+
+]]--
