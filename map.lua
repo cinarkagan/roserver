8f696d6fda84215be5184b69769bea5f429e9731
cinarkagan <68916102+cinarkagan@users.noreply.github.com>
2022-12-24 19:44:01 +0300

    Update map.lua

--- map.lua
+++ map.lua
@@ -3,5 +3,6 @@ Baseplate.Name = 'Baseplate'
 Baseplate.Anchored = true
 Baseplate.CanCollide = true
 Baseplate.Size = Vector3.new(1756.770263671875,20,2048)
+Baseplate.BrickColor = BrickColor.new('Dark stone grey')
 Baseplate.Position = Vector3.new(0,0,0)
 Baseplate.Parent = workspace
